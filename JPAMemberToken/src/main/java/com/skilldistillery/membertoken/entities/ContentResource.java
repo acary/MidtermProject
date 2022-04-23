02d5143570d5f90ef0437409defd472506029355
Julian Chae <Julianchae9@gmail.com>
2022-04-23 11:10:56 -0600

    purchase entity, and purchase junit

--- JPAMemberToken/src/main/java/com/skilldistillery/membertoken/entities/ContentResource.java
+++ JPAMemberToken/src/main/java/com/skilldistillery/membertoken/entities/ContentResource.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.membertoken.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,11 +12,59 @@ public class ContentResource {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(name= "content_id")
+	private int contentId;
 
 	private String title;
 
 	private String description;
-
+	@Column(name= "resource_url")
 	private String resourceUrl;
+	
+	public ContentResource() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getContentId() {
+		return contentId;
+	}
+
+	public void setContentId(int contentId) {
+		this.contentId = contentId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getResourceUrl() {
+		return resourceUrl;
+	}
+
+	public void setResourceUrl(String resourceUrl) {
+		this.resourceUrl = resourceUrl;
+	}
+	
+	
 	
 }
---
+++ JPAMemberToken/src/test/java/com/skilldistillery/membertoken/entities/ContentResourceTest.java
@@ -0,0 +1,59 @@
+package com.skilldistillery.membertoken.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ContentResourceTest {
+	
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private ContentResource contentResource;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAMemberToken");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		contentResource = em.find(ContentResource.class, 2);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		contentResource = null; 
+	}
+
+	@Test
+	@DisplayName("testing purchase mapping user id")
+	void test1() {
+		assertNotNull(contentResource);
+		assertEquals("https://www.nike.com/launch/t/air-jordan-1-stash", contentResource.getResourceUrl());
+
+	}
+	
+
+
+	
+}
+
+	
