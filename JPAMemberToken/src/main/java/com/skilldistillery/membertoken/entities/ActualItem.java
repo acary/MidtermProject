606c3e57ad148d6d89da501652a3f594f48cf633
breckiam <breckiam@gmail.com>
2022-04-23 10:48:50 -0600

    ActualItem and Business mapped to db, g&s added

--- JPAMemberToken/src/main/java/com/skilldistillery/membertoken/entities/ActualItem.java
+++ JPAMemberToken/src/main/java/com/skilldistillery/membertoken/entities/ActualItem.java
@@ -1,5 +1,8 @@
 package com.skilldistillery.membertoken.entities;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,5 +19,67 @@ public class ActualItem {
 
 	private String description;
 
+	@Column(name="img_url")
 	private String imageUrl;
+	
+	public ActualItem() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	@Override
+	public String toString() {
+		return "ActualItem [id=" + id + ", name=" + name + ", description=" + description + ", imageUrl=" + imageUrl
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ActualItem other = (ActualItem) obj;
+		return id == other.id;
+	}
+	
+	
 }
--- JPAMemberToken/src/main/java/com/skilldistillery/membertoken/entities/Business.java
+++ JPAMemberToken/src/main/java/com/skilldistillery/membertoken/entities/Business.java
@@ -1,7 +1,8 @@
 package com.skilldistillery.membertoken.entities;
 
-import java.time.LocalDateTime;
+import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,9 +18,79 @@ public class Business {
 	private String name;
 
 	private String description;
-
+	
+	@Column(name="logo_url")
 	private String logoUrl;
 
 	private String imageUrl;
+	
+	public Business() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLogoUrl() {
+		return logoUrl;
+	}
+
+	public void setLogoUrl(String logoUrl) {
+		this.logoUrl = logoUrl;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	@Override
+	public String toString() {
+		return "Business [id=" + id + ", name=" + name + ", description=" + description + ", logoUrl=" + logoUrl
+				+ ", imageUrl=" + imageUrl + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Business other = (Business) obj;
+		return id == other.id;
+	}
+	
+	
 
 }
