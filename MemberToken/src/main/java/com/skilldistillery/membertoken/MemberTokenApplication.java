baf2fa28b205af8f9382ca7dbacfea8752cda5b7
Andy Cary <info@andycary.com>
2022-04-28 16:00:20 -0400

    Configure app for deployment

--- MemberToken/src/main/java/com/skilldistillery/membertoken/MemberTokenApplication.java
+++ MemberToken/src/main/java/com/skilldistillery/membertoken/MemberTokenApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.membertoken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MemberTokenApplication {
+public class MemberTokenApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MemberTokenApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemberTokenApplication.class, args);
